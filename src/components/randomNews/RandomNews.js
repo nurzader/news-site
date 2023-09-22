9a32c0876014c3645cdf73bd4f9658cd0d996cde
Nurzada <nurzaderr@gmail.com>
2023-09-22 15:14:55 +0600

    Добавлен индикатор во время загрузки данных. Использован спиннер для информирования пользователей о процессе загрузки.

--- src/components/randomNews/RandomNews.js
+++ src/components/randomNews/RandomNews.js
@@ -1,35 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import './randomNews.scss';
 import NewsService from '../../services/NewsService';
+import Spinner from '../spinner/Spinner';
 
 const RandomNews = () => {
   const [news, setNews] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     updateNews();
-    const timerId = setInterval(updateNews, 70000)
+
+    const timerId = setInterval(updateNews, 70000);
 
     return () => {
-      clearInterval(timerId)
-    }
+      clearInterval(timerId);
+    };
   }, []);
 
   const { getNew } = NewsService();
 
   const onNewsLoaded = (news) => {
+    setLoading(false);
     setNews(news);
+  };
+
+  const onNewsLoading = () => {
+    setLoading(true);
   };
 
   const updateNews = () => {
     const id = Math.floor(Math.random() * (47 - 1) + 1);
+
+    onNewsLoading();
     getNew(id).then(onNewsLoaded);
   };
 
-  console.log(news);
-  const { img_url, title, country, published_date, summary } = news;
+  const spinner = loading ? <Spinner /> : null;
+  const content = !(loading || !news) ? <View news={news} /> : null;
 
   return (
     <article className="random-news">
+      {spinner}
+      {content}
+    </article>
+  );
+};
+
+export default RandomNews;
+
+const View = ({ news }) => {
+  const { img_url, title, country, published_date, summary } = news;
+
+  return (
+    <>
       <div className="random-news__banner">
         <img className="random-news__img" src={img_url} alt="banner" />
         <p className="random-news__title">{title}</p>
@@ -39,8 +62,6 @@ const RandomNews = () => {
         </div>
       </div>
       <div className="random-news__description">{summary}</div>
-    </article>
+    </>
   );
 };
-
-export default RandomNews;
